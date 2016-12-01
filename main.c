0b1ea2e59b4e8ff19d3c76440aa21f9b6fd5aad9
sad mac <staticsn0w@users.noreply.github.com>
2016-12-01 18:28:07 -0500

    Update main.c
    credit to ypwn for help

--- main.c
+++ main.c
@@ -3,56 +3,127 @@
 // under the GPLv2-License (SEE LICENSE.md) //
 
 #include <stdio.h>
-
-int main(void) {
-
-// init tape //
-char array[] = {0};
-char *ptr=array;
-
-x = 0 
+#define ARRSIZE 30000 // TODO: make dynamic
  
-// open file "main.bfm" //
-FILE *fp;
-fp=fopen("main.bfm", "r");
-
-while  ( ( x = fgetc( file ) ) != EOF )
-{
- if (x == 76) {
-   ++ptr;
- }
+//char input[] = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
+char* input;
+char* array;
+int counter = 0;
  
- else if (x == 74) {
-   --ptr;
- }
- else if (x == 53) {
-   ++*ptr;
- }
+int loopmode = 0;
+int beginloop = 0;
  
- else if (x == 55) {
-   --*ptr;
- }
-  
- else if (x == 56) {
-   putchar(*ptr);
- }
-  
- else if (x == 54) {
-   *ptr=getchar();
- }
-  
- else if (x == 133) {
-   while (*ptr) {
- }
+int debugprintarray() {
+	printf("\nDEBUG: [");
+	for (int i = 0; i < ARRSIZE; ++i) {
+		if (i == ARRSIZE-1)
+			printf("\"%c\" = %d", array[i], array[i]);
+		else
+			printf("\"%c\" = %d, ", array[i], array[i]);
+	}
+	printf("]\n");
  
- else if (x == 135) {
-   }
- }
- 
- else if (x == 41) {
-   break;
- }
+	return 0;
 }
-
-exit(0);
+ 
+int interpret(char x, int i) {
+	//printf("Interpreting '%c' at pos %d\n", x, i);
+	switch (x) {
+		case '>':
+			++counter;
+			break;
+		case '<':
+			--counter;
+			break;
+		case '+':
+			++(array[counter]);
+			//printf("Char at pos %d is now %d\n", counter, array[counter]);
+			break;
+		case '-':
+			--(array[counter]);
+			//printf("Char at pos %d is now %d\n", counter, array[counter]);
+			break;
+		case '.':
+			printf("%c", array[counter]);
+			break;
+		case ',':
+			array[counter] = getchar();
+			break;
+		case '[':
+			loopmode = 1;
+			beginloop = i;
+			break;
+		//case ']':
+		//	loopmode = 0;
+		//	break;
+		case '#': // debugging command, prints out the tape
+			debugprintarray();
+			break;
+		default:
+			break;
+	}
+ 
+	return 0;
+}
+ 
+void runbrain(char* code, int size) {
+	//printf("Code: %s, size: %d\n", code, size);
+	printf("Output: ");
+	for (int i = 0; i < size; ++i) {
+		//printf("I: %d, char: %c\n", i, code[i]);
+		interpret(code[i], i);
+		if (loopmode) {
+			if (code[i] == ']') {
+				if (array[counter]) {
+					i = beginloop;
+					//printf("array counter: %d %d\n", array[counter], counter);
+				}
+				else
+					loopmode = 0;
+			}
+		}
+	}
+	printf("\nProgram finished.\n");
+}
+ 
+int main(int argc, char** argv) {
+	if (argc <= 1) {
+		printf("brainfvck-modified interactive mode\n");
+		while (1) {
+			char* str = (char*)calloc(1, ARRSIZE);
+			//memset(str, 0, ARRSIZE);
+			printf("> ");
+			gets(str);
+			if (str[0] == '\0')
+				goto end;
+			array = (char*)calloc(1, ARRSIZE);
+			runbrain(str, strlen(str));
+			free(array);
+			free(str);
+		}
+	}
+ 
+	// let's read the file in argument 1
+	FILE* myfile = fopen(argv[1], "rb");
+ 
+	// obtain file size
+	fseek(myfile, 0, SEEK_END);
+	int filesize = ftell(myfile);
+	rewind(myfile);
+ 
+	// load the file into "input"
+	input = (char*)malloc(filesize+1);
+	fread(input, 1, filesize, myfile);
+	input[filesize] = '\0';
+	fclose(myfile);
+	//printf("Now input contains: %s\n", input);
+ 
+	array = (char*)calloc(1, ARRSIZE);
+	//int size = sizeof(input)-1;
+	int size = filesize;
+ 
+	runbrain(input, filesize);
+ 
+end:
+	return 0;
 }
