c14f444c787db476b500589f841058b2c408f32d
sad mac <staticsn0w@users.noreply.github.com>
2016-12-03 13:06:16 -0500

    Update main.c

--- main.c
+++ main.c
@@ -59,22 +59,36 @@ int interpret(char x, int i) {
 			loopmode = 1;
 			beginloop = i;
 			break;
-                case '!':
-                        printf("exiting...\n");
-                        exit(0);
-                        break;
+		case '!':
+			printf("exiting...\n");
+			exit(0);
+			break;
 		case '#': // debugging command, prints out the tape
 			if (debug == 1) {
 				debugprintarray();
 				break;
 			}
+			else {
+				break;
+			}
 		case '@': // debugging command, frees array
 			if (debug == 1) {
 				free(array);
+				printf("tape cleared.\n");
 				break;
 			}
+			else {
+				break;
+			}
+		case '/':
+			system("clear");
+			break;
+		case 'v': // version command
+			printf("bfm v1\n");
+			break;
 		case '?': // help command
-			printf ("\nbfm - an expanded brainfuck interpreter.\n\n> - move pointer right.\n< - move pointer left.\n+ - add 1 to current cell.\n- - subtract 1 from current cell.\n. - print contents of current cell in ASCII.\n, - ask for one char of user input.\n! - end program.\n# - print contents of tape.\n@ - clear tape.\n? - shows this help dialogue.\n");
+			printf("\nbfm - an expanded brainfuck interpreter.\n\n> - move pointer right.\n< - move pointer left.\n+ - add 1 to current cell.\n- - subtract 1 from current cell.\n. - print contents of current cell in ASCII.\n, - ask for one char of user input.\n! - end program.\n# - print contents of tape.\n@ - clear tape.\n/ - clear screen.\nv - show version\n? - shows this help dialogue.\n");
+			break;
 		default:
 			break;
 	}
@@ -98,6 +112,7 @@ void runbrain(char* code, int size) {
 					if (debug == 1) {
 						printf("array counter: %d %d\n", array[counter], counter);
 					}
+					printf("\n")
 				}
 				else
 					loopmode = 0;
@@ -105,59 +120,65 @@ void runbrain(char* code, int size) {
 		}
 	}
 }
- 
-int main(int argc, char** argv) {
-	if (argc <= 1) {
-		printf("brainfvck-modified interactive shell\n");
-		while (1) {
-			char* str = (char*)calloc(1, ARRSIZE);
-			//memset(str, 0, ARRSIZE);
-			printf("> ");
-			gets(str);
-			if (str[0] == '\0')
-				goto end;
-			array = (char*)calloc(1, ARRSIZE);
-			runbrain(str, strlen(str));
-			free(array);
-			free(str);
-		}
+
+void runshell(int isdebug) {
+	if (!isdebug)
+		printf("bfm interactive shell.\n");
+	else {
+		printf("bfm interactive shell. (debug mode)\n");
+		debug = 1;
 	}
-	
-	if (!strcmp(argv[2],"-d")) {
-		printf("brainfvck-modified interactive shell. (debug mode)\n");
-                while (1) {
-		      debug = 1;
+
+	while (1) {
 		char* str = (char*)calloc(1, ARRSIZE);
-		printf("> ");
-		gets(str);
+		//memset(str, 0, ARRSIZE);
+		printf("bfm> ");
+		fgets(str, ARRSIZE, stdin);
+		if (str[0] == '\0')
+			return;
 		array = (char*)calloc(1, ARRSIZE);
 		runbrain(str, strlen(str));
-		// don't free array, use @ to manually free it.
-                free(array);
+		free(array);
 		free(str);
-                }
 	}
+}
+
+void runfile(char* filename) {
 	// let's read the file in argument 1
-	FILE* myfile = fopen(argv[1], "rb");
- 
+	FILE* myfile = fopen(filename, "rb");
+
 	// obtain file size
 	fseek(myfile, 0, SEEK_END);
 	int filesize = ftell(myfile);
 	rewind(myfile);
- 
+
 	// load the file into "input"
 	input = (char*)malloc(filesize+1);
 	fread(input, 1, filesize, myfile);
 	input[filesize] = '\0';
 	fclose(myfile);
 	//printf("Now input contains: %s\n", input);
- 
+
 	array = (char*)calloc(1, ARRSIZE);
 	//int size = sizeof(input)-1;
 	int size = filesize;
- 
+
 	runbrain(input, filesize);
+}
  
+int main(int argc, char** argv) {
+	for (int i = 0; i < argc; ++i) {
+		if (!strcmp(argv[i], "-d")) {
+			runshell(1);
+			goto end;
+		}
+	}
+
+	if (argc == 2)
+		runfile(argv[1]);
+	else
+		runshell(0);
+
 end:
 	return 0;
 }
